bec8264525044abfe0fd46daf396fa3669210bdf
Timothy Morris <timlmorris@pm.me>
2025-07-27 14:18:41 -0400

    attempting keyvault access one after making change in azure portal to permissions

--- server/util.ts
+++ server/util.ts
@@ -1,16 +1,15 @@
 import { DefaultAzureCredential, ClientSecretCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 
-// Replace with your Key Vault name
-const vaultName = "kv-timorris"; 
-const url = `https://${vaultName}.vault.azure.net`;
+const url = process.env.KEYVAULT_URL || '';
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const tenantId = process.env.TENANT_ID;
 
 if (!tenantId || !clientId || !clientSecret) {
     throw new Error('Missing required environment variables for Azure authentication');
-  }
+}
+
 const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 
 //const credential = new DefaultAzureCredential();
