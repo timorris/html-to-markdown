2e7af30707321b2a69c58e4b67f670aed1d05299
Timothy Morris <timlmorris@pm.me>
2025-07-27 04:01:51 -0400

    trying to get to kv another way

--- server/routes.ts
+++ server/routes.ts
@@ -3,7 +3,33 @@ import { createServer, type Server } from "http";
 import { storage } from "./storage";
 import { z } from "zod";
 import { Resend } from 'resend';
-import { getSecret } from "./util";
+//import { getSecret } from "./util";
+import { DefaultAzureCredential, ClientSecretCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+
+// Replace with your Key Vault name
+const vaultName = "kv-timorris"; 
+const url = `https://${vaultName}.vault.azure.net`;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const tenantId = process.env.TENANT_ID;
+
+if (!tenantId || !clientId || !clientSecret) {
+    throw new Error('Missing required environment variables for Azure authentication');
+  }
+const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+//const credential = new DefaultAzureCredential();
+const client = new SecretClient(url, credential);
+
+async function getSecret(secretName: string): Promise<string | undefined> {
+  try {
+    const secret = await client.getSecret(secretName);
+    return secret.value || undefined;
+  } catch (error) {
+    return undefined;
+  }
+}
 
 export async function registerRoutes(app: Express): Promise<Server> {
   // GitHub API test endpoint
