d08a571bcc74326620855926469b682be0fb93d6
Timothy Morris <timlmorris@pm.me>
2025-07-27 03:39:14 -0400

    attempting to connect to keyvault with sp

--- server/util.ts
+++ server/util.ts
@@ -1,11 +1,19 @@
-import { DefaultAzureCredential } from "@azure/identity";
+import { DefaultAzureCredential, ClientSecretCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 
 // Replace with your Key Vault name
 const vaultName = "kv-timorris"; 
 const url = `https://${vaultName}.vault.azure.net`;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const tenantId = process.env.TENANT_ID;
 
-const credential = new DefaultAzureCredential();
+if (!tenantId || !clientId || !clientSecret) {
+    throw new Error('Missing required environment variables for Azure authentication');
+  }
+const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+//const credential = new DefaultAzureCredential();
 const client = new SecretClient(url, credential);
 
 export async function getSecret(secretName: string): Promise<string | undefined> {
